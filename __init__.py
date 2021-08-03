95813e399678d44f1f1883836a073ffcbc850fa0
jake <jacoby8s@gmail.com>
2021-08-03 18:49:05 -0400

    import mitchelllab data

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .utils import ensure_dir
---
+++ utils.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import time
+
+
+def reporthook(count, block_size, total_size):
+    global start_time
+    if count == 0:
+        start_time = time.time()
+        return
+    duration = time.time() - start_time
+    progress_size = int(count * block_size)
+    speed = int(progress_size / (1024 * duration))
+    percent = int(count * block_size * 100 / total_size)
+    sys.stdout.write("\r...%d%%, %d MB, %d KB/s, %d seconds passed" %
+                    (percent, progress_size / (1024 * 1024), speed, duration))
+    sys.stdout.flush()
+
+def ensure_dir(dir_name: str):
+    '''
+    Creates folder if not exists.
+    '''
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
