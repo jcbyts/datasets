e96b90e68028382eb19573a145e0de116d6ece8a
jake <jacoby8s@gmail.com>
2021-08-04 12:27:14 -0400

    utilities for downloading from urls

--- utils.py
+++ utils.py
@@ -2,7 +2,14 @@ import os
 import sys
 import time
 
-
+def download_file(url: str, file_name: str):
+    '''
+    Downloads file from url and saves it to file_name.
+    '''
+    import urllib.request
+    print("Downloading %s to %s" % (url, file_name))
+    urllib.request.urlretrieve(url, file_name, reporthook)
+    
 def reporthook(count, block_size, total_size):
     global start_time
     if count == 0:
